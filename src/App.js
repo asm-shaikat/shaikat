3139c3794c1afc4511151c67d4a054d9b5029874
asm-shaikat <asmshaikat1952@gmail.com>
2024-10-13 02:25:05 +0600

    Tailwind and daisyui added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Abu Shadat Shaikat</h1>
-      <h2>CI/CD test successfull</h2>
+      <div className='text-center'>
+      <h1 className='font-semibold text-red-700'>Abu Shadat Shaikat</h1>
+      <h2 className='text-yellow-300'>CI/CD test successfull</h2>
+      </div>
     </>
   );
 }
